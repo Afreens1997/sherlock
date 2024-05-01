af05abd8296698e65f70d2ce5484e99989162073
ShrikaraVarna <svarna@andrew.cmu.edu>
2024-05-01 11:59:45 -0400

    Attention improvement using Blurred Images

--- multimodal_baseline_BLIP/run.sh
+++ multimodal_baseline_BLIP/run.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=globalCap_train
-#SBATCH --error=globalCap_train.err
-#SBATCH --output=globalCap_train.out
+#SBATCH --job-name=blurBLIP
+#SBATCH --error=blurBLIP.err
+#SBATCH --output=blurBLIP.out
 #SBATCH --mem=60G
 #SBATCH --gres=gpu:A6000:2
 #SBATCH --time=1-00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=svarna@andrew.cmu.edu
-# torchrun --nproc_per_node 1 --master_port 25678 finetuningBLIP.py ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/subset_train.json \
-#        ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/subset_train.json \
-#        --workers_dataloader 16 \
-#        --lr .00001 \
-#        --batch_size 64 \
-#        --n_epochs 5 \
+torchrun --nproc_per_node 1 --master_port 25678 BLIPBlurred.py ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/subset_train.json \
+       ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/subset_train.json \
+       ../../../../data/tir/projects/tir7/user_data/vishwavs/mmml_2024/blurred_images \
+       --workers_dataloader 16 \
+       --lr .00001 \
+       --batch_size 64 \
+       --n_epochs 2 \
+
+# torchrun --nproc_per_node 1 --master_port 25678 BLIPGlobal.py ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/subset_train.json \
+#        ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/multimodal_baselines/inferences/globalCapBLIP_train.json \
 #        --vg_dir ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/VG \
 #        --vcr_dir ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/VCR
-
-torchrun --nproc_per_node 1 --master_port 25678 BLIPGlobal.py ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/subset_train.json \
-       ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/multimodal_baselines/inferences/globalCapBLIP_train.json \
-       --vg_dir ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/VG \
-       --vcr_dir ../../../../data/tir/projects/tir4/users/svarna/Sherlock/data/VCR
 
 # python BLIPZeroShot.py
 
